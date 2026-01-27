ebc1be4faecacadac437c820a31ede03a0a793ef
Janod Abesekara <125848373+JanodAbesekara@users.noreply.github.com>
2026-01-27 11:58:22 +0530

    Add test

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -5,7 +5,15 @@ test('First Playwright test', async  () =>
     //playwright code
 });
 
-test('Second Playwright test', async  () =>
-{
-    //playwright code
+test.only("Page01Test2", async ({ page }) => {
+  await page.goto("https://example.com");
+  await page.title();
+  //  css
+  await page.locator("#nonexistent").type("Hello"); // Old version: await page.fill("#nonexistent", "Hello");
+  await page.locator("#nonexistent").fill("Hello");
+  await page.locator("[type='password']").fill("world");
+  await page.locator("#submit").click();
+  // wait until this locator shows up page
+  console.log(await page.locator("[style*='block']").textContent());
+  await expect(page.locator("[style*='block']")).toContainText("Incorrect");
 });
